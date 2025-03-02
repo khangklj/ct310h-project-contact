69571ed1127ef1eaedff2c3dcb9f55373ab29fb6
Khang <khangklj@gmail.com>
2025-03-02 19:23:58 +0700

    Add auth management and init login form

---
+++ ct310h-project-contact/frmLogin.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ct310h_project_contact
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(357, 272);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Name = "frmLogin";
+            Text = "frmLogin";
+            FormClosed += frmLogin_FormClosed;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnLogin;
+    }
+}
--- ct310h-project-contact/frmTestDatabase.cs
+++ ct310h-project-contact/frmTestDatabase.cs
@@ -18,10 +18,28 @@ namespace ct310h_project_contact
                 }
                 clsDatabase.CloseConnection();
                 MessageBox.Show("Connect to database successfully!");
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
                 return;
+            }
+        }
+
+        private void frmTestDatabase_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnTestAuth_Click(object sender, EventArgs e)
+        {
+            int? accountID = AuthInfo.AccountID;
+            if (accountID == null)
+            {
+                MessageBox.Show("Can't retrieve auth info");
+            } else
+            {
+                MessageBox.Show($"You are logged with id = {accountID}");
             }
         }
     }

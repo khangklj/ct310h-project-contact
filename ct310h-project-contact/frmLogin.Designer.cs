eb6cedd4f5dfc0c0e51fe0dfa7c6afd0bc5d1838
Tran Duy <tqduy2k3@gmail.com>
2025-03-05 17:22:45 +0700

    Login

--- ct310h-project-contact/frmLogin.Designer.cs
+++ ct310h-project-contact/frmLogin.Designer.cs
@@ -29,31 +29,118 @@
         private void InitializeComponent()
         {
             btnLogin = new Button();
+            label1 = new Label();
+            btnExit = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            linkLabel1 = new LinkLabel();
             SuspendLayout();
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(357, 272);
+            btnLogin.Location = new Point(389, 254);
+            btnLogin.Margin = new Padding(3, 2, 3, 2);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(94, 29);
+            btnLogin.Size = new Size(82, 22);
             btnLogin.TabIndex = 2;
             btnLogin.Text = "Login";
             btnLogin.UseVisualStyleBackColor = true;
             btnLogin.Click += btnLogin_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(307, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 32);
+            label1.TabIndex = 3;
+            label1.Text = "LOGIN";
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(236, 253);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 4;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(236, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(239, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(342, 126);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(146, 23);
+            txtUsername.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(342, 172);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(146, 23);
+            txtPassword.TabIndex = 8;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(320, 291);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(88, 15);
+            linkLabel1.TabIndex = 9;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "No account yet";
+            // 
             // frmLogin
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(linkLabel1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(btnExit);
+            Controls.Add(label1);
             Controls.Add(btnLogin);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "frmLogin";
             Text = "frmLogin";
             FormClosed += frmLogin_FormClosed;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private Button btnLogin;
+        private Label label1;
+        private Button btnExit;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private LinkLabel linkLabel1;
     }
 }
--- ct310h-project-contact/frmLogin.cs
+++ ct310h-project-contact/frmLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,21 +20,77 @@ namespace ct310h_project_contact
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            // Implement the login function correctly
-            // ....
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
 
-            // This is for testing only
-            // =================
-            AuthInfo.SetAuth(2);
-            frmTestDatabase frm = new frmTestDatabase();
-            this.Hide();
-            frm.ShowDialog();
-            // =================
+            int? userId = ValidateUser(username, password);
+
+            if (userId.HasValue)
+            {
+                AuthInfo.SetAuth(userId.Value); // Gán ID người dùng vào AuthInfo
+                MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                frmTestDatabase frm = new frmTestDatabase();
+                this.Hide();
+                frm.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void frmLogin_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
         }
+        private int? ValidateUser(string username, string password)
+        {
+            try
+            {
+                // Kiểm tra kết nối với cơ sở dữ liệu
+                if (!clsDatabase.OpenConnection())
+                {
+                    MessageBox.Show("Cannot connect to database.");
+                    return null;
+                }
+
+                string query = "SELECT Account_ID FROM Account WHERE Account_UserName = @username AND Account_Password = @password";
+
+                using (SqlCommand cmd = new SqlCommand(query, clsDatabase.conn))
+                {
+                    // Tránh SQL Injection bằng cách khai báo rõ kiểu dữ liệu
+                    cmd.Parameters.Add("@username", SqlDbType.NVarChar, 100).Value = username;
+                    cmd.Parameters.Add("@password", SqlDbType.NVarChar, 100).Value = password;
+
+                    object result = cmd.ExecuteScalar();
+                    return result != null ? (int?)Convert.ToInt32(result) : null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                clsDatabase.CloseConnection();
+            }
+        }
+
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
 }

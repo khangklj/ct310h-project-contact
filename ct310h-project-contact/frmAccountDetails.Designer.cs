adcb62804a6596b1bd00f0c6e81c1dbef6128362
Tran Duy <tqduy2k3@gmail.com>
2025-03-15 20:35:07 +0700

    add frmAccountDetails

---
+++ ct310h-project-contact/frmAccountDetails.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ct310h_project_contact
+{
+    partial class frmAccountDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            picAvatar = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            txtName = new TextBox();
+            btnSave = new Button();
+            btnCancel = new Button();
+            ((System.ComponentModel.ISupportInitialize)picAvatar).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(284, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 25);
+            label1.TabIndex = 0;
+            label1.Text = "AccountDetail";
+            // 
+            // picAvatar
+            // 
+            picAvatar.Location = new Point(284, 90);
+            picAvatar.Name = "picAvatar";
+            picAvatar.Size = new Size(157, 109);
+            picAvatar.SizeMode = PictureBoxSizeMode.Zoom;
+            picAvatar.TabIndex = 1;
+            picAvatar.TabStop = false;
+            picAvatar.Click += picAvatar_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.Location = new Point(173, 226);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 19);
+            label2.TabIndex = 2;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.Location = new Point(173, 328);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 19);
+            label3.TabIndex = 3;
+            label3.Text = "Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F);
+            label4.Location = new Point(173, 281);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 19);
+            label4.TabIndex = 4;
+            label4.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.BorderStyle = BorderStyle.None;
+            txtUsername.Location = new Point(284, 226);
+            txtUsername.Name = "txtUsername";
+            txtUsername.ReadOnly = true;
+            txtUsername.Size = new Size(166, 16);
+            txtUsername.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(284, 281);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(166, 23);
+            txtPassword.TabIndex = 6;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(284, 328);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(166, 23);
+            txtName.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new Font("Segoe UI", 10F);
+            btnSave.Location = new Point(415, 393);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(90, 30);
+            btnSave.TabIndex = 8;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Font = new Font("Segoe UI", 10F);
+            btnCancel.Location = new Point(220, 393);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(89, 30);
+            btnCancel.TabIndex = 9;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // frmAccountDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(695, 532);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(txtName);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(picAvatar);
+            Controls.Add(label1);
+            Name = "frmAccountDetails";
+            Text = "frmAccountDetails";
+            ((System.ComponentModel.ISupportInitialize)picAvatar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox picAvatar;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private TextBox txtName;
+        private Button btnSave;
+        private Button btnCancel;
+    }
+}
